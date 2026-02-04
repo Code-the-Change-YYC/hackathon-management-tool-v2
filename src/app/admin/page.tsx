1b84a5f2283dbe9bf48036f613a4df85c92cbfb7
Harshita Jindia <125141251+HarshitaJindia19@users.noreply.github.com>
2026-02-03 23:22:14 -0700

    started the algorithm for schedule. Loop through teams, and assign them available rooms. Jump to the next time block if all rooms full.

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,16 +1,21 @@
 import { requireRole } from "@/server/better-auth/auth-helpers/helpers";
 import { Role } from "@/types/types";
+import { mockTeams, mockRooms } from "@/app/admin/mockData";
+import { createSchedule } from "@/app/admin/scheduler";
 import styles from "../dashboard.module.scss";
 
 export default async function AdminPage() {
-  const session = await requireRole([Role.ADMIN]);
+  //const session = await requireRole([Role.ADMIN]);
+
+  const startTime = new Date("2026-01-31T10:00:00");
+  const mySchedule = createSchedule(mockTeams, mockRooms, startTime, 15);
 
   return (
     <main className={styles.main}>
       <header className={styles.header}>
         <h1 className={styles.title}>Hackathon Admin</h1>
         <div>
-          <span>{session.user.name}</span>
+          {/* <span>{session.user.name}</span> */}
           <span className={styles.roleBadge}>Admin</span>
         </div>
       </header>
@@ -19,6 +24,14 @@ export default async function AdminPage() {
           <h2 className={styles.welcome}>Welcome back, Admin!</h2>
           <p>Manage your hackathon settings, users, and rounds here.</p>
         </div>
+        <h1 className={styles.welcome}>Schedule</h1>
+        <ul>
+          {mySchedule.map((timeSlot, index) => (
+            <li key={index}>
+              {timeSlot.teamName} is in {timeSlot.roomName} with {timeSlot.judgeName} at {timeSlot.start.toLocaleTimeString()}
+            </li>
+          ))}
+        </ul>
       </div>
     </main>
   );
