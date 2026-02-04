c1c33bd44f3f27ae8e811ce91c04ebdc06b1d899
sim <simarkandola51@gmail.com>
2026-02-03 21:07:47 -0700

    fix auth schema and db push

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,13 +3,13 @@ import type { Config } from "drizzle-kit";
 import { env } from "@/env";
 
 export default {
-	schema: [
-		"./src/server/db/schema.ts", // data schema
-		"./src/server/db/auth-schema.ts", // auth schema
-	],
-	dialect: "postgresql",
-	dbCredentials: {
-		url: env.DATABASE_URL,
-	},
-	tablesFilter: ["hackathon-management-tool-v2_*"],
+  schema: [
+    "./src/server/db/schema.ts", // data schema
+    "./src/server/db/auth-schema.ts", // auth schema
+  ],
+  dialect: "postgresql",
+  dbCredentials: {
+    url: env.DATABASE_URL,
+  },
+  tablesFilter: ["hackathon_*"],
 } satisfies Config;
