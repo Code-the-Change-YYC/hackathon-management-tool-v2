08781c227c74e3dc755009bbe8f742c79e37dd06
Harshita Jindia <125141251+HarshitaJindia19@users.noreply.github.com>
2026-02-03 23:22:14 -0700

    started the algorithm for schedule. Loop through teams, and assign them available rooms. Jump to the next time block if all rooms full.

---
+++ src/app/admin/scheduler.ts
@@ -0,0 +1,29 @@
+
+export function createSchedule(teams: any[], rooms: any[], startTime: Date, duration: number) {
+    let schedule: { teamName: string; roomName: string; judgeName: string, start: Date; end: Date; }[] = [];
+    let currentTime = new Date(startTime);
+    let roomIndex = 0;
+    let bufferTime = 5;
+
+    teams.forEach((team) => {
+        const timeSlot = {
+            teamName: team.name,
+            roomName: rooms[roomIndex].name,
+            judgeName: rooms[roomIndex].judge,
+            start: new Date(currentTime),
+            end: new Date(currentTime.getTime() + (duration * 60000)),
+        };
+
+        schedule.push(timeSlot);
+
+        roomIndex++;
+
+        if (roomIndex >= rooms.length) {
+            roomIndex = 0;
+            currentTime = new Date(currentTime.getTime() + (duration + bufferTime) * 60000)
+        }
+    })
+
+    return schedule;
+}
+
