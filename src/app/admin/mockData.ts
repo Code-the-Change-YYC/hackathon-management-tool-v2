7c6e0c6670ac6d09329905ff759caeb8ca0a1957
Harshita Jindia <125141251+HarshitaJindia19@users.noreply.github.com>
2026-02-03 23:22:14 -0700

    started the algorithm for schedule. Loop through teams, and assign them available rooms. Jump to the next time block if all rooms full.

---
+++ src/app/admin/mockData.ts
@@ -0,0 +1,15 @@
+export const mockTeams = [
+    { id: "1", name: "Team Alpha" },
+    { id: "2", name: "Team Beta" },
+    { id: "3", name: "Team Gamma" },
+    { id: "4", name: "Team Delta" },
+    { id: "5", name: "Team Epsilon" },
+];
+
+export const mockRooms = [
+    { id: "r1", name: "Room 101", judge: "Simar" },
+    { id: "r2", name: "Room 102", judge: "Anthony" },
+    // { id: "r3", name: "Room 103" },
+    // { id: "r4", name: "Room 104" },
+    // { id: "r5", name: "Room 105" },
+];
