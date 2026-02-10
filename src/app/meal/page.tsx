6419d1e15737940a203f951a1e1ed484e6a25fe1
justine <justinemangaliman@gmail.com>
2026-02-10 00:02:10 -0700

    feat: scanUserIn feature
    implemented a scanUserIn public procedure and added a section in my
    meals page to scan in a user for testing purposes

--- src/app/meal/page.tsx
+++ src/app/meal/page.tsx
@@ -7,10 +7,19 @@ export default function MealPage() {
 	const [title, setTitle] = useState("");
 	const [startTime, setStartTime] = useState("");
 	const [endTime, setEndTime] = useState("");
+	const [mealId, setMealId] = useState("");
+	const [userId, setUserId] = useState("");
+	const [checkedInBy, setCheckedInBy] = useState("");
+
 	const createMeal = api.meals.addMeal.useMutation();
+	const scanUserIn = api.meals.scanUserIn.useMutation();
 
 	function handleCreateMeal() {
 		createMeal.mutate({ title, startTime, endTime });
+	}
+
+	function handleScanUserIn() {
+		scanUserIn.mutate({ mealId, userId, checkedInBy });
 	}
 
 	return (
@@ -28,14 +37,14 @@ export default function MealPage() {
 						onChange={(e) => setTitle(e.target.value)}
 						type="text"
 					/>
-					<label htmlFor="title">Start time:</label>
+					<label htmlFor="start-time">Start time:</label>
 					<input
 						id="start-time"
 						name="start-time"
 						onChange={(e) => setStartTime(e.target.value)}
 						type="datetime-local"
 					/>
-					<label htmlFor="title">End time:</label>
+					<label htmlFor="end-time">End time:</label>
 					<input
 						id="end-time"
 						name="end-time"
@@ -45,6 +54,38 @@ export default function MealPage() {
 					<button
 						onClick={() => {
 							handleCreateMeal();
+						}}
+						type="button"
+					>
+						Submit
+					</button>
+				</div>
+				<div>
+					<h2>Scan user in for a meal</h2>
+					<label htmlFor="meal-id">Meal Id:</label>
+					<input
+						id="meal-id"
+						name="meal-id"
+						onChange={(e) => setMealId(e.target.value)}
+						type="text"
+					/>
+					<label htmlFor="user-id">User Id:</label>
+					<input
+						id="start-time"
+						name="start-time"
+						onChange={(e) => setUserId(e.target.value)}
+						type="text"
+					/>
+					<label htmlFor="checked-in-by">Checked In By:</label>
+					<input
+						id="checked-in-by"
+						name="checked-in-by"
+						onChange={(e) => setCheckedInBy(e.target.value)}
+						type="text"
+					/>
+					<button
+						onClick={() => {
+							handleScanUserIn();
 						}}
 						type="button"
 					>
