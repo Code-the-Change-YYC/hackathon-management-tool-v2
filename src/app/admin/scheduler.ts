32a05d9c2728b69378fe67932b4491ad04e19f78
Harshita Jindia <125141251+HarshitaJindia19@users.noreply.github.com>
2026-02-17 15:49:01 -0700

    feat: modified scheduler to match generateSchedule procedure

--- src/app/admin/scheduler.ts
+++ src/app/admin/scheduler.ts
@@ -1,59 +1,41 @@
 type Team = {
   id: string;
-  name: string;
 };
 
-type Room = {
+type Judge = {
   id: string;
-  name: string;
-  judge: string;
 };
 
-type TimeSlot = {
+type AssignmentSlot = {
   teamId: string;
-  teamName: string;
-  roomId: string;
-  roomName: string;
-  judgeName: string;
+  judgeId: string;
   start: Date;
-  end: Date;
 };
-
 
 export function createSchedule(
 	teams: Team[],
-	rooms: Room[],
+	judges: Judge[],
 	startTime: Date,
   duration: number,
   bufferTime: number  = 5
-) {
-  const schedule: TimeSlot[] = [];
+): AssignmentSlot[] {
+
+
+  const schedule: AssignmentSlot[] = [];
   
   let currentTime = new Date(startTime);
   
-  // Process team in batches equal to number of rooms
-  for (let i = 0; i < teams.length; i += rooms.length) {  
+  for (let i = 0; i < teams.length; i++) {  
+    
+    const team = teams[i]!;
+    const judge = judges[i % judges.length]!;
 
-    const batch = teams.slice(i, i + rooms.length);
+    schedule.push({
+      teamId: team.id,
+      judgeId: judge.id,
+      start: new Date(currentTime),
+    });
 
-    batch.forEach((team, index) => {
-      
-      const room = rooms[index];
-
-      if (!room) return;
-
-      schedule.push({
-        teamId: team.id,
-        teamName: team.name,
-        roomId: room.id,
-        roomName: room.name,
-        judgeName: room.judge,
-        start: new Date(currentTime),
-        end: new Date(currentTime.getTime() + duration * 60000),
-      });
-    })
-
-    // Move time forward after batch completes
     currentTime = new Date(
       currentTime.getTime() + (duration + bufferTime) * 60000
     );
