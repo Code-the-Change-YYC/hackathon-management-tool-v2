133fc8e00a8b2572db4d7244e2a64b14d9925c57
justine <justinemangaliman@gmail.com>
2026-02-19 19:25:32 -0700

    chore: change drizzle config to pattern match schema files

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,11 +3,7 @@ import type { Config } from "drizzle-kit";
 import { env } from "@/env";
 
 export default {
-	schema: [
-		"./src/server/db/schema.ts", // data schema
-		"./src/server/db/auth-schema.ts", // auth schema
-		"./src/server/db/meal-schema.ts" // meal schema
-	],
+	schema: ["./src/server/db/*schema.ts"],
 	dialect: "postgresql",
 	dbCredentials: {
 		url: env.DATABASE_URL
