91f5e8f0a3adfda134d013333085d125dccb6587
Harshita Jindia <125141251+HarshitaJindia19@users.noreply.github.com>
2026-04-06 23:32:01 -0600

    feat: added the sponsors section

---
+++ src/app/components/admin/landingpage/data/sponsors.ts
@@ -0,0 +1,13 @@
+export type Sponsor = {
+	id: string;
+	image: string;
+	name: string;
+};
+
+export const sponsors: Sponsor[] = [
+	{ id: "sponsor-1", image: "/svgs/CTCLogo.svg", name: "Sponsor 1" },
+	{ id: "sponsor-2", image: "/svgs/CTCLogo.svg", name: "Sponsor 2" },
+	{ id: "sponsor-3", image: "/svgs/CTCLogo.svg", name: "Sponsor 3" },
+	{ id: "sponsor-4", image: "/svgs/CTCLogo.svg", name: "Sponsor 4" },
+	{ id: "sponsor-5", image: "/svgs/CTCLogo.svg", name: "Sponsor 5" }
+];
