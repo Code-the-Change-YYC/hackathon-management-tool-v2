0fd1b7287ddffb8f8979b0e7d8f73c9af3771c8d
Harshita Jindia <125141251+HarshitaJindia19@users.noreply.github.com>
2026-06-21 16:47:41 -0600

    feat: added links and icons to footer

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -8,27 +8,36 @@ export default function Footer() {
 			<div className="flex flex-col items-center gap-2">
 				<p className="font-bold text-sm text-white">Keep up with us!</p>
 				<div className="flex items-center gap-5">
-					<Link href="https://facebook.com" target="_blank">
+					<Link
+						href="https://www.facebook.com/CodeTheChangeYYC/"
+						target="_blank"
+					>
 						<Image
 							alt="Facebook"
 							height={iconDimension}
-							src="/svgs/facebook.svg"
+							src="/svgs/socialIcons/facebook_icon.svg"
 							width={iconDimension}
 						/>
 					</Link>
-					<Link href="https://instagram.com" target="_blank">
+					<Link
+						href="https://www.instagram.com/codethechangeyyc/?hl=en"
+						target="_blank"
+					>
 						<Image
 							alt="Instagram"
 							height={iconDimension}
-							src="/svgs/instagram.svg"
+							src="/svgs/socialIcons/instagram_icon.svg"
 							width={iconDimension}
 						/>
 					</Link>
-					<Link href="https://linkedin.com" target="_blank">
+					<Link
+						href="linkedin.com/company/codethechangeyyc/?originalSubdomain=ca"
+						target="_blank"
+					>
 						<Image
 							alt="LinkedIn"
 							height={iconDimension}
-							src="/svgs/linkedin.svg"
+							src="/svgs/socialIcons/linkedin_icon.svg"
 							width={iconDimension}
 						/>
 					</Link>
@@ -36,21 +45,21 @@ export default function Footer() {
 						<Image
 							alt="YouTube"
 							height={iconDimension}
-							src="/svgs/youtube.svg"
+							src="/svgs/socialIcons/youtube_icon.svg"
 							width={iconDimension}
 						/>
 					</Link>
-					<Link href="https://github.com" target="_blank">
+					<Link href="https://github.com/Code-the-Change-YYC" target="_blank">
 						<Image
 							alt="GitHub"
 							height={iconDimension}
-							src="/svgs/github.svg"
+							src="/svgs/socialIcons/github_icon.svg"
 							width={iconDimension}
 						/>
 					</Link>
 				</div>
 
-				<p className="text-dashboard-grey/70 text-sm">
+				<p className="pb-10 text-dashboard-grey/70 text-sm">
 					Copyright © Code The Change YYC
 				</p>
 			</div>
