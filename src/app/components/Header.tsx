b2ea16ee06e845286b0212f9c134e6823c936a5b
Harshita Jindia <125141251+HarshitaJindia19@users.noreply.github.com>
2026-06-30 17:36:38 -0600

    Updated header UI

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,28 +1,26 @@
 import Image from "next/image";
 import Link from "next/link";
+import type { HeaderProps } from "@/types/landingPage";
 
-type HeaderProps = {
-	isSignedIn: boolean;
-	hasTeam: boolean;
-};
+//TODO: Fix Sign In logic (add profile pic when signed in)
 
 export default function Header({ isSignedIn, hasTeam }: HeaderProps) {
 	return (
-		<header className="relative w-full border-light-grey border-b bg-white shadow-sm">
-			<div className="mx-auto flex w-full max-w-screen-2x1 items-center px-6 py-5">
-				<div className="min-w-25 text-awesomer-purple hover:text-awesome-purple">
+		<header className="relative w-full bg-white shadow-[0_4px_4px_0_rgba(0,0,0,0.25)]">
+			<div className="flex items-center justify-between px-[88px] py-[40px]">
+				<div className="min-w-[100px]">
 					{!isSignedIn && (
 						<Link
-							className="font-semibold text-base transition-colors"
-							href="/login"
+							className="!text-awesomer-purple hover:!text-awesome-purple font-semibold text-2xl transition-colors"
+							href="/register"
 						>
 							Join Hackathon
 						</Link>
 					)}
 					{isSignedIn && !hasTeam && (
 						<Link
-							className="font-semibold text-base transition-colors"
-							href="#"
+							className="!text-awesomer-purple hover:!text-awesome-purple font-semibold text-2xl transition-colors"
+							href="/teams"
 						>
 							Join a Team
 						</Link>
@@ -32,14 +30,19 @@ export default function Header({ isSignedIn, hasTeam }: HeaderProps) {
 				<div className="-translate-x-1/2 absolute left-1/2">
 					<Image
 						alt="CTC logo"
-						height={40}
+						height={70}
 						src="/svgs/CTCLogo.svg"
-						width={40}
+						width={70}
 					/>
 				</div>
 
 				<div className="flex min-w-[100px] justify-end">
-					{/* Add profile pic */}
+					<Link
+						className="!text-awesomer-purple hover:!text-awesome-purple font-semibold text-2xl transition-colors"
+						href="/login"
+					>
+						Sign In
+					</Link>
 				</div>
 			</div>
 		</header>
