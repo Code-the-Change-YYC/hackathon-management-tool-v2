567b7ba4c6433a611f88f90fb4eda1cb9440c377
Justin Pham <justin.phamx4@gmail.com>
2026-08-05 00:12:44 -0600

    feat: implement Contentful types and fetch function

--- src/server/contentful/index.ts
+++ src/server/contentful/index.ts
@@ -1,7 +1,25 @@
-import "server-only";
 import * as contentful from "contentful";
+import "server-only";
+import type { BaseEntry } from "contentful";
+import type { ContentTypeMap } from "@/types/contentfulTypes";
 
-export const contentfulClient = contentful.createClient({
+const contentfulClient = contentful.createClient({
 	space: process.env.CONTENTFUL_SPACE_ID ?? "",
 	accessToken: process.env.CONTENTFUL_ACCESS_TOKEN ?? ""
 });
+interface ContentfulEntry<T> extends BaseEntry {
+	contentTypeId: string;
+	fields: T;
+}
+
+export async function fetchContentful<T extends keyof ContentTypeMap>(
+	contentId: T
+): Promise<ContentfulEntry<ContentTypeMap[T]>[]> {
+	const res = await contentfulClient.getEntries<
+		ContentfulEntry<ContentTypeMap[T]>
+	>({
+		content_type: contentId
+	});
+	const data = res.items;
+	return data as ContentfulEntry<ContentTypeMap[T]>[];
+}
