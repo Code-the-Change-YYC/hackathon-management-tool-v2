563b5a1937c628db372d969d0fa469f44f9b0f08
Justin Pham <113923596+justin-phxm@users.noreply.github.com>
2026-08-10 14:05:04 -0600

    Upgrade to next16 (#47)
    * upgrade to nextjs 16
    
    * refactor: simplify Next.js build and development scripts

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,10 @@
-import path from "node:path";
-
 import type { NextConfig } from "next";
 
 import "./src/env.js";
 
 const config: NextConfig = {
-	webpack: (webpackConfig) => {
-		webpackConfig.resolve.alias["@"] = path.resolve(process.cwd(), "src");
-		return webpackConfig;
+	experimental: {
+		useTypeScriptCli: true
 	}
 };
 
