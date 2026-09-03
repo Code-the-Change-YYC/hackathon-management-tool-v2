30ba65b6839ed1751ca42fd4fe43706baf479159
vilyvil <furyliving@gmail.com>
2026-09-03 15:52:55 -0600

    Add authentication guards to new admin routes

--- src/app/admin/users/page.tsx
+++ src/app/admin/users/page.tsx
@@ -1,6 +1,10 @@
 import UserTable from "@/app/components/admin/usertable/UserTable";
+import { requireRole } from "@/server/better-auth/auth-helpers/helpers";
+import { Role } from "@/types/types";
 
-export default function Users() {
+export default async function Users() {
+	await requireRole([Role.ADMIN]);
+
 	return (
 		<main className="flex size-full flex-1 flex-col overflow-y-auto bg-white">
 			<UserTable />
